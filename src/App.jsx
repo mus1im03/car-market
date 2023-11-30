e2ca3ee93bd15f372aaa40d63c58f8b9f302fb05
Halifat <hanif.001@mail.ru>
2023-11-30 16:59:34 +0300

    card done

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import MainContent from "./components/MainContent/MainContent";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import CarsCategory from "./components/CarsCategory/CarsCategory";
+import Card from "./components/cards/card";
 
 const App = () => {
   return (
     <>
       <Header />
+      {/* <Card /> */}
       <CarsCategory />
       <Routes>
         {/* <Route path="/" element={<MainContent />} /> */}
         {/* <Route path="/" element={<CarsCategory />} /> */}
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
